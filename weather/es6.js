97f6c63c7d2a1c7711df5d04e9dab694b525d24d
shunwuyu <shunwu2001@163.com>
2018-10-15 16:09:19 +0800

    工作流之babel

---
+++ weather/es6.js
@@ -0,0 +1,11 @@
+const num = 9;
+let isMarried = false //唐唐
+
+let yourTurn = ['好工作', '找老板谈', '驴妈妈网'].map((task, index) => {
+  return `${index}-${task}`
+});
+
+console.log(yourTurn);
+
+// es6=> es5
+// var
